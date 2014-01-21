797bbe2c055bc2db687fbcae4136e0f78b38b190
Dr. Philip Wenig <philip.wenig@gmx.net>
2014-01-21 19:30:14 +0100

    The enhanced wizard is used now.

--- openchrom/plugins/net.openchrom.chromatogram.msd.process.supplier.groovy.ui/src/net/openchrom/chromatogram/msd/process/supplier/groovy/ui/wizards/GroovyScriptWizard.java
+++ openchrom/plugins/net.openchrom.chromatogram.msd.process.supplier.groovy.ui/src/net/openchrom/chromatogram/msd/process/supplier/groovy/ui/wizards/GroovyScriptWizard.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2014 Philip (eselmeister) Wenig.
+ * Copyright (c) 2014 Dr. Philip Wenig.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -7,122 +7,39 @@
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- * openchrom - initial API and implementation
+ * Dr. Philip Wenig - initial API and implementation
  *******************************************************************************/
 package net.openchrom.chromatogram.msd.process.supplier.groovy.ui.wizards;
-
-import org.eclipse.jface.operation.IRunnableWithProgress;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.wizard.Wizard;
-import org.eclipse.ui.INewWizard;
-import org.eclipse.ui.IWorkbench;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.ide.IDE;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.InvocationTargetException;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.core.resources.IContainer;
+
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IWorkspaceRoot;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.core.runtime.Status;
+import net.openchrom.logging.core.Logger;
+import net.openchrom.support.ui.wizards.AbstractFileWizard;
 
-/**
- * This is a sample new wizard. Its role is to create a new file
- * resource in the provided container. If the container resource
- * (a folder or a project) is selected in the workspace
- * when the wizard is opened, it will accept it as the target
- * container. The wizard creates one file with the extension
- * "groovy". If a sample multi-page editor (also available
- * as a template) is registered for the same extension, it will
- * be able to open it.
- */
-public class GroovyScriptWizard extends Wizard implements INewWizard {
+public class GroovyScriptWizard extends AbstractFileWizard {
 
-	private GroovyScriptWizardPage page;
-	private ISelection selection;
+	private static final Logger logger = Logger.getLogger(GroovyScriptWizard.class);
 
-	/**
-	 * Constructor for GroovyScriptWizard.
-	 */
 	public GroovyScriptWizard() {
 
-		super();
-		setNeedsProgressMonitor(true);
+		super("GroovyScript", ".groovy");
 	}
 
-	/**
-	 * Adding the page to the wizard.
-	 */
-	public void addPages() {
+	@Override
+	public void doFinish(IProgressMonitor monitor) throws CoreException {
 
-		page = new GroovyScriptWizardPage(selection);
-		addPage(page);
-	}
-
-	/**
-	 * This method is called when 'Finish' button is pressed in
-	 * the wizard. We will create an operation and run it
-	 * using wizard as execution context.
-	 */
-	public boolean performFinish() {
-
-		final String containerName = page.getContainerName();
-		final String fileName = page.getFileName();
-		IRunnableWithProgress op = new IRunnableWithProgress() {
-
-			public void run(IProgressMonitor monitor) throws InvocationTargetException {
-
-				try {
-					doFinish(containerName, fileName, monitor);
-				} catch(CoreException e) {
-					throw new InvocationTargetException(e);
-				} finally {
-					monitor.done();
-				}
-			}
-		};
+		monitor.beginTask("Create Groovy Script", IProgressMonitor.UNKNOWN);
+		final IFile file = super.prepareProject(monitor);
+		/*
+		 * Initialize a simple batch process job.
+		 */
 		try {
-			getContainer().run(true, false, op);
-		} catch(InterruptedException e) {
-			return false;
-		} catch(InvocationTargetException e) {
-			Throwable realException = e.getTargetException();
-			MessageDialog.openError(getShell(), "Error", realException.getMessage());
-			return false;
-		}
-		return true;
-	}
-
-	/**
-	 * The worker method. It will find the container, create the
-	 * file if missing or just replace its contents, and open
-	 * the editor on the newly created file.
-	 */
-	private void doFinish(String containerName, String fileName, IProgressMonitor monitor) throws CoreException {
-
-		// create a sample file
-		monitor.beginTask("Creating " + fileName, 2);
-		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-		IResource resource = root.findMember(new Path(containerName));
-		if(!resource.exists() || !(resource instanceof IContainer)) {
-			throwCoreException("Container \"" + containerName + "\" does not exist.");
-		}
-		IContainer container = (IContainer)resource;
-		final IFile file = container.getFile(new Path(fileName));
-		try {
-			InputStream stream = openContentStream();
+			InputStream stream = new ByteArrayInputStream("Groovy Script".getBytes());
 			if(file.exists()) {
 				file.setContents(stream, true, true, monitor);
 			} else {
@@ -130,46 +47,12 @@ public class GroovyScriptWizard extends Wizard implements INewWizard {
 			}
 			stream.close();
 		} catch(IOException e) {
+			logger.warn(e);
 		}
-		monitor.worked(1);
-		monitor.setTaskName("Opening file for editing...");
-		getShell().getDisplay().asyncExec(new Runnable() {
-
-			public void run() {
-
-				IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-				try {
-					IDE.openEditor(page, file, true);
-				} catch(PartInitException e) {
-				}
-			}
-		});
-		monitor.worked(1);
-	}
-
-	/**
-	 * We will initialize file contents with a sample text.
-	 */
-	private InputStream openContentStream() {
-
-		String contents = "This is the initial file contents for *.groovy file that should be word-sorted in the Preview page of the multi-page editor";
-		return new ByteArrayInputStream(contents.getBytes());
-	}
-
-	private void throwCoreException(String message) throws CoreException {
-
-		IStatus status = new Status(IStatus.ERROR, "net.openchrom.chromatogram.msd.process.supplier.groovy.ui", IStatus.OK, message, null);
-		throw new CoreException(status);
-	}
-
-	/**
-	 * We will accept the selection in the workbench to see if
-	 * we can initialize from it.
-	 * 
-	 * @see IWorkbenchWizard#init(IWorkbench, IStructuredSelection)
-	 */
-	public void init(IWorkbench workbench, IStructuredSelection selection) {
-
-		this.selection = selection;
+		/*
+		 * Refresh
+		 */
+		super.refreshWorkspace(monitor);
+		super.runOpenEditor(file, monitor);
 	}
 }
