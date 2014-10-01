f9bb56e13690b96cf8964f4ce83c0f04e869ddd9
Dr. Philip Wenig <philip.wenig@openchrom.net>
2014-10-01 19:12:05 +0200

    The project will be part of the OpenChrom release again.

---
+++ openchrom/plugins/net.openchrom.chromatogram.msd.process.supplier.groovy.ui/src/net/openchrom/chromatogram/msd/process/supplier/groovy/ui/editors/GroovyScriptEditor.java
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2012, 2014 Philip (eselmeister) Wenig.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * chemclipse - initial API and implementation
+ *******************************************************************************/
+package net.openchrom.chromatogram.msd.process.supplier.groovy.ui.editors;
+
+import org.eclipse.ui.texteditor.AbstractDecoratedTextEditor;
+
+public class GroovyScriptEditor extends AbstractDecoratedTextEditor {
+}
---
+++ openchrom/plugins/net.openchrom.chromatogram.msd.process.supplier.groovy.ui/src/net/openchrom/chromatogram/msd/process/supplier/groovy/ui/preferences/PreferencePage.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) 2012, 2014 Philip (eselmeister) Wenig.
+ * 
+ * All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Philip (eselmeister) Wenig - initial API and implementation
+ *******************************************************************************/
+package net.openchrom.chromatogram.msd.process.supplier.groovy.ui.preferences;
+
+import org.eclipse.jface.preference.BooleanFieldEditor;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.eclipse.ui.IWorkbench;
+
+import net.chemclipse.support.ui.preferences.fieldeditors.SpacerFieldEditor;
+import net.openchrom.chromatogram.msd.process.supplier.groovy.preferences.ProcessPreferences;
+import net.openchrom.chromatogram.msd.process.supplier.groovy.ui.Activator;
+
+public class PreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
+
+	public PreferencePage() {
+
+		super(GRID);
+		setPreferenceStore(Activator.getDefault().getPreferenceStore());
+		setDescription("Script Support.");
+	}
+
+	/**
+	 * Creates the field editors. Field editors are abstractions of the common
+	 * GUI blocks needed to manipulate various types of preferences. Each field
+	 * editor knows how to save and restore itself.
+	 */
+	public void createFieldEditors() {
+
+		addField(new SpacerFieldEditor(getFieldEditorParent()));
+		addField(new BooleanFieldEditor(ProcessPreferences.P_USE_FILE_DIALOG, "Use a file selection dialog instead of the active editor.", getFieldEditorParent()));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * org.eclipse.ui.IWorkbenchPreferencePage#init(org.eclipse.ui.IWorkbench)
+	 */
+	public void init(IWorkbench workbench) {
+
+	}
+}
