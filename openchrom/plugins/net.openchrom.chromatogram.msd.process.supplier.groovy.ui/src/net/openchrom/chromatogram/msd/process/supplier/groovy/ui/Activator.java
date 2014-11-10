0af166897ac0a13eb6ac8590545c0423de9146cc
Dr. Philip Wenig <philip.wenig@openchrom.net>
2014-11-10 13:25:10 +0100

    The model Activator class has been refactored.

--- openchrom/plugins/net.openchrom.chromatogram.msd.process.supplier.groovy.ui/src/net/openchrom/chromatogram/msd/process/supplier/groovy/ui/Activator.java
+++ openchrom/plugins/net.openchrom.chromatogram.msd.process.supplier.groovy.ui/src/net/openchrom/chromatogram/msd/process/supplier/groovy/ui/Activator.java
@@ -19,8 +19,6 @@ import org.osgi.framework.BundleContext;
  */
 public class Activator extends AbstractUIPlugin {
 
-	// The plug-in ID
-	public static final String PLUGIN_ID = "net.openchrom.chromatogram.msd.process.supplier.groovy.ui"; //$NON-NLS-1$
 	// The shared instance
 	private static Activator plugin;
 
